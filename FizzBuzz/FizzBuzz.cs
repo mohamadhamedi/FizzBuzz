7996ab25fe33effa675abc5b270b59b9e65b6856
MohamadHamedi <mohamadhamedi@yahoo.com>
2020-12-19 20:45:43 +0330

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public static string GetOutputString(int inputNumber)
+        {
+            if ((inputNumber % 3) == 0 && (inputNumber % 5) == 0)
+                return "FizzBuzz";
+            if ((inputNumber % 3) == 0)
+                return "Fizz";
+            if ((inputNumber % 5) == 0)
+                return "Buzz";
+            return inputNumber.ToString();
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputKey = Console.ReadLine();
+            Console.WriteLine(FizzBuzz.GetOutputString(int.Parse(inputKey)));
+            Console.ReadKey();
+
+        }
+    }
+}
